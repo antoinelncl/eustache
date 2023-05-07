f3479766a1d79a75c4d085388aad44632af1fdf3
Antoine <antoine@skello.io>
2023-05-07 10:47:06 +0200

    feat: add ping command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,2 +1,4 @@
+export * from '~/commands/ask';
 export * from '~/commands/hello';
+export * from '~/commands/ping';
 export * from '~/commands/reload-commands';
---
+++ src/commands/ping.ts
@@ -0,0 +1,17 @@
+import { SlashCommandBuilder } from 'discord.js';
+import { helloSettings as settings } from '~/config/command-settings';
+import { errorHelper } from '~/helpers/error-helper';
+import { Command } from '~/types/command';
+
+export const ping: Command = {
+  data: new SlashCommandBuilder().setName('ping').setDescription('get eustache current ping').setDMPermission(true),
+  run: async function (interaction): Promise<void> {
+    if (!interaction.isChatInputCommand()) return;
+    try {
+      const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
+      interaction.editReply(`Roundtrip latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
+    } catch (err) {
+      errorHelper(settings.errorContext, err);
+    }
+  },
+};
